b714f05df2c147ae517002e6b966e5d608abc54a
Dencik <agafonov.denis11@gmail.com>
2024-05-27 12:01:43 +0300

    Commit Initial

---
+++ src/main/java/org/dencik_incorporated/App.java
@@ -0,0 +1,14 @@
+package org.dencik_incorporated;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Wrapper wi = new Wrapper(1000);
+        System.out.println(wi.getValue());
+    }
+}
---
+++ src/main/java/org/dencik_incorporated/Wrapper.java
@@ -0,0 +1,18 @@
+package org.dencik_incorporated;
+
+public class Wrapper {
+    private Integer value;
+
+    public Wrapper(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
