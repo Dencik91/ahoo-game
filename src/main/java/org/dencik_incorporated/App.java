4d318b6c7ba2353c3a150f6a804da538c12366d8
Dencik <agafonov.denis11@gmail.com>
2024-05-29 10:40:23 +0300

    Commit Initial

--- src/main/java/org/dencik_incorporated/App.java
+++ src/main/java/org/dencik_incorporated/App.java
@@ -1,15 +1,35 @@
 package org.dencik_incorporated;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Wrapper<Integer> wi = new Wrapper<>(1000);
-        Wrapper<String> ws = new Wrapper<>("abc");
-        System.out.println(ws.getValue().toUpperCase());
+import org.dencik_incorporated.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+        List<User> users = new ArrayList<>(); //SO[L]I[D]
+
+
+
+        while(users.size()<5) {
+            users.add(new User(String.format("user %d",users.size()+1)));
+        }
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+        System.out.println("users: " + users.size());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introdu pozitia: ");
+        int i = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Introdu username ");
+        String value = scanner.nextLine();
+
+        users.get(i).setUserName(value);
+
+        users.forEach(System.out::println);
+
     }
 }
