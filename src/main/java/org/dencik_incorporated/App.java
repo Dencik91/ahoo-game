836f461daa7b1b38bbcd37aba88b55615e9acc19
Dencik <agafonov.denis11@gmail.com>
2024-05-31 19:18:51 +0300

    initial commit

---
+++ src/main/java/org/dencik_incorporated/App.java
@@ -0,0 +1,21 @@
+package org.dencik_incorporated;
+
+import java.io.File;
+import java.io.FileInputStream;
+import org.json.JSONObject;
+import org.w3c.dom.ls.LSOutput;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = new FileInputStream(new File(System.getProperty("user.dir") + "\\data\\message.json"));
+
+        String textContent = new String(fis.readAllBytes());
+
+        JSONObject jo = new JSONObject(textContent);
+
+        System.out.println(jo.getString("body"));
+
+        // de transmis
+    }
+
+}
